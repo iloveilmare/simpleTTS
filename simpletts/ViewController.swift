34cf78e40a6ee84ba79987d6c6a094ec4d065d10
kangsangmook <tech4human.ksm@gmail.com>
2019-04-22 03:12:01 +0900

    init

--- simpletts/ViewController.swift
+++ simpletts/ViewController.swift
@@ -9,11 +9,20 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
+    @IBOutlet weak var checkBoxJp: NSButton!
+    @IBOutlet weak var checkBoxKor: NSButton!
+    @IBOutlet var clearButton: NSView!
+    @IBOutlet weak var messageField: NSTextField!
+    @IBOutlet weak var sayButton: NSButton!
+    var speech : NSSpeechSynthesizer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        speech = NSSpeechSynthesizer.init()
     }
 
     override var representedObject: Any? {
@@ -21,7 +30,39 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    
+    @IBAction func onCheckBoxKorClicked(_ sender: Any) {
+        checkBoxJp?.state = NSControl.StateValue.off
+        
+        let voices = NSSpeechSynthesizer.availableVoices
+        voices.filter { voice in voice.rawValue == "com.apple.speech.synthesis.voice.yuna.premium" }
+            .map{ yuna in speech.setVoice(yuna)}
 
-
+    }
+    
+    @IBAction func onCheckBoxJpClicked(_ sender: Any) {
+        checkBoxKor?.state = NSControl.StateValue.off
+        
+        let voices = NSSpeechSynthesizer.availableVoices
+        voices.filter{ voice in voice.rawValue == "com.apple.speech.synthesis.voice.kyoko" }
+            .map { kyoko in speech.setVoice(kyoko)}
+    }
+    
+    @IBAction func onClearButtonClicked(_ sender: Any) {
+        messageField.stringValue = ""
+    }
+    
+    @IBAction func onSayButtonClicked(_ sender: NSButton) {
+        let message = messageField.stringValue
+        print(message)
+        
+        let voices = NSSpeechSynthesizer.availableVoices
+        voices.forEach{ voice in
+            print(voice.rawValue)
+        }
+        
+        speech.startSpeaking(message)
+    }
+    
 }
 
