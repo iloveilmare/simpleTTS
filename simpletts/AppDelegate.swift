dad9de3720d0796898eb799bc8d48d5e3e010594
kangsangmook <tech4human.ksm@gmail.com>
2019-04-21 23:54:40 +0900

    Initial Commit

---
+++ simpletts/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  simpletts
+//
+//  Created by 강상묵 on 21/04/2019.
+//  Copyright © 2019 sang-mook.kang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ simpletts/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  simpletts
+//
+//  Created by 강상묵 on 21/04/2019.
+//  Copyright © 2019 sang-mook.kang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
